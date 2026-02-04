114528833846a3b510f38f753c31821412e24e5a
03obyk-cmd <252697084+03obyk-cmd@users.noreply.github.com>
2026-02-04 12:43:38 +0000

    [Finish]ファイルの準備まで

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :users, only: [:new, :create] , path_names: { new: 'sign_up' }
+  root to: "homes#top"
+  resources :books, only: [:new, :index, :show]
+  resources :users, only: [:new, :create, :show, :edit] , path_names: { new: 'sign_up' }
 
   resource :session
   resources :passwords, param: :token
