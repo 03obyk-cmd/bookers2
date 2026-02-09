e86238ec47d838ba29b2e6e3690b56d3549422aa
03obyk-cmd <252697084+03obyk-cmd@users.noreply.github.com>
2026-02-09 11:07:14 +0000

    [Finish]テンプレート化まで

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,14 +6,20 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     @book.user_id = Current.user.id
-    @book.save
-    redirect_to book_path
+    if @book.save
+      redirect_to book_path(@book)
+    else
+      render :index , status: :unprocessable_entity
+    end
   end
 
   def index
+    @books = Book.all
+    @book  = Book.new
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   private
